eb096b59755be3bb6265dbba0c22af746c03a312
Ugo Riboni <nerochiaro@gmail.com>
2022-11-04 23:31:59 +0100

    Ditto as before

--- tunnel.py
+++ tunnel.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 def open_tunnel(port):
-    tunnel = subprocess.Popen(f"node tunnel.js ${port}", shell=True, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    tunnel = subprocess.Popen(f"node tunnel.js {port}", shell=True, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # TODO: We should probably care about this subprocess, somehow, when running locally
 
     time.sleep(1)  # TODO: ideally we should monitor the file and when it appears and is closed read it.
