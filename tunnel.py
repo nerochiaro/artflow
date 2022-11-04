9daa46e07a7309945d8f0f7fa70ca3d29ac03e5d
Ugo Riboni <nerochiaro@gmail.com>
2022-11-05 00:31:09 +0100

    Attempt to install localtunnel if we are running in colab

--- tunnel.py
+++ tunnel.py
@@ -1,8 +1,11 @@
 import subprocess
 import time
-from environment import tunnel_dir
+from environment import tunnel_dir, is_colab
 
 def open_tunnel(port):
+    if is_colab():
+        subprocess.call("npm ci", shell=True)
+
     tunnel = subprocess.Popen(f"node tunnel.js {port} {tunnel_dir}", shell=True, bufsize=0, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # TODO: We should probably care about this subprocess, somehow, when running locally
 
